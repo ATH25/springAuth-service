765f39711201e5093a7364d76af321554aa097d5
AJUTHOMAS <aju_thomas@yahoo.com>
2017-07-01 21:44:03 -0400

    adding all files. Second commit

---
+++ src/test/java/com/thomas/auth/SpringAuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thomas.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAuthServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
